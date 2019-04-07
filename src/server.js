d7e193602a0626452ab51f557681d5ed4a09335a
pezhmanparsaee <p.parsaee@gmail.com>
2019-04-07 21:27:34 +0430

    include default port number for express app

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const app = require('./app');
 const config = require('./config');
+const port = config.app.port || 3000;
 
 const server = http.createServer(app);
 
-server.listen(config.app.port, ()=> {
+server.listen(port, ()=> {
   const addr = server.address();
   console.log(`API is running at ${addr.address} and port number ${config.app.port}`);
-})
+});
