1dde849ffecda76e83e57392d28858e2e23143f1
pezhmanparsaee <p.parsaee@gmail.com>
2019-04-07 05:46:34 +0430

    add new middlewares for error handling and error logging

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const startup = require('./app-start/startup');
 const routes = require('./api/v1/index');
+const logErrors = require('./infrastructures/middlewares/log-errors');
+const clientErrorHandler = require('./infrastructures/middlewares/client-error-handler');
 
 const app = express();
 
 startup(app);
 routes(app);
+logErrors(app);
+clientErrorHandler(app);
 
 module.exports = app;
