2a995f533a44f77fcc3d3a6930fd37885b8f0ae9
pezhmanparsaee <p.parsaee@gmail.com>
2020-05-15 01:51:57 +0430

    close mongodb connection in SIGINT process event

--- src/server.js
+++ src/server.js
@@ -3,11 +3,20 @@ const http = require('http');
 const app = require('./app');
 const config = require('./config');
 const port = config.app.port || 3000;
+const dbContext = require('./data-layer/db-context');
 
 const server = http.createServer(app);
 
-server.listen(port, ()=> {
+server.listen(port, () => {
   const addr = server.address();
   console.log(`port: ${port}`);
   console.log(`API is running at ${addr.address} and port number ${port}`);
+});
+
+process.on('SIGINT', async () => {
+  const { connection } = await dbContext.connect();
+  console.log('connection', connection);
+  await connection.close();
+  console.log('Mongodb connections was closed on app termination');
+  process.exit();
 });
