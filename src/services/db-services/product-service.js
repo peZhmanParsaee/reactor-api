98e06d0294f25e70ff3cff9db43b9087f0487204
pezhmanparsaee <p.parsaee@gmail.com>
2020-05-26 12:47:23 +0430

    convert ProductService class to a closure

--- src/services/db-services/product-service.js
+++ src/services/db-services/product-service.js
@@ -2,8 +2,8 @@ const dbContext = require('../../data-layer/db-context');
 const opStatusGenerator = require('../../infrastructures/helpers/op-status-generator');
 const { COLLECTIONS } = require('../../infrastructures/models/enums.json');
 
-class ProductService {
-  async getAll() {
+export default (function ProductService() {
+  async function getAll() {
     const { db } = await dbContext.connect();
     const res = await db.collection(COLLECTIONS.PRODUCTS)
       .find().toArray();
@@ -14,7 +14,7 @@ class ProductService {
     });
   }
 
-  async add(product) {
+  async function add(product) {
     const { db } = await dbContext.connect();
       
     const res = await db.collection(COLLECTIONS.PRODUCTS)
@@ -24,9 +24,11 @@ class ProductService {
       status: res.result.ok === 1,
       payload: res.result.ok === 1 ? res.ops[0] : null
     });
-    
   }
-  
-}
 
-module.exports = ProductService;
+  return {
+    getAll,
+    add
+  };
+  
+})();
