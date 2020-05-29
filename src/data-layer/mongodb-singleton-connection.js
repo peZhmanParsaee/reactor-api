28ac04a7812f84f873e2317bfbe198b063f12377
pezhmanparsaee <p.parsaee@gmail.com>
2020-05-29 19:53:38 +0430

    create a singleton generator for mongodb connection

---
+++ src/data-layer/mongodb-singleton-connection.js
@@ -0,0 +1,42 @@
+const MongoClient = require('mongodb').MongoClient;
+const config = require('../config');
+
+module.exports = (function() {
+  let connectionInstance;
+  let db;
+
+  function getInstance() {
+    return new Promise(function(resolve, reject) {
+      if (connectionInstance) {
+        return resolve(connectionInstance);
+      }
+  
+      const options = {
+        useNewUrlParser: true
+      };
+      MongoClient.connect(config.db.url, options, function(err, client) {
+        if (err) {
+          return reject(err);
+        }
+        
+        connectionInstance = client;
+        db = client.db(config.db.name);
+
+        return resolve(connectionInstance);
+      });
+    })
+  }
+
+  function getDb() {
+    if (!db) {
+      throw new Error('db object is not initialized!');
+    }
+    
+    return db;
+  }
+
+  return {
+    getInstance,
+    getDb
+  };
+})();
