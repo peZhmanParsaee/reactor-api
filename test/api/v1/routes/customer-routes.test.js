9d90b0b2143e5f6a9eabb0a7e1921899dbf366d0
pezhmanparsaee <p.parsaee@gmail.com>
2020-05-31 23:59:16 +0430

    add a unit tes for GET /search

--- test/api/v1/routes/customer-routes.test.js
+++ test/api/v1/routes/customer-routes.test.js
@@ -3,10 +3,29 @@ const expect = require('chai').expect;
 const app = require('../../../../src/app');
 
 describe('Customer routes test /api/v1/customer', function () {
+
   describe('GET /', function () {
     it('should return all customers', function (done) {
       supertest(app)
         .get('/api/v1/customer')
+        .expect('Content-Type', /application\/json/)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(err).to.equal(null);
+
+          expect(res.status).to.equal(200);
+          expect(Array.isArray(res.body.payload)).to.equal(true);
+          expect(res.body.status).to.equal(true);
+          
+          done();
+        });
+    });
+  });
+
+  describe('GET /search', function () {
+    it('should serach some customers', function (done) {
+      supertest(app)
+        .get('/api/v1/customer/search')
         .expect('Content-Type', /application\/json/)
         .end((err, res) => {
           if (err) done(err);
