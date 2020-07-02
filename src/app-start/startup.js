03a96a5d8886dc545489a2516905b2d428597d09
pezhmanparsaee <p.parsaee@gmail.com>
2020-07-03 00:09:13 +0430

    remove expressValidator middleware

--- src/app-start/startup.js
+++ src/app-start/startup.js
@@ -1,7 +1,6 @@
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('../infrastructures/middlewares/cors');
-const expressValidator = require('express-validator');
 
 module.exports = app => {
   app.use(cors());
@@ -9,38 +8,4 @@ module.exports = app => {
   app.use(bodyParser.json());
 
   app.use(morgan('dev'));
-
-  app.use(
-    expressValidator({
-      customValidators: {
-        lessThan: (value1, value2) => {
-          return value1 < value2;
-        },
-        lessThanOrEqual: (value1, value2) => {
-          return value1 <= value2;
-        },
-        isArray: value => {
-          return Array.isArray(value) && value.length > 0;
-        },
-        isString: value => {
-          return typeof value === 'string';
-        },
-        eachIsEmpty: (values, prop) => {
-          let notValid = false;
-          for (let i = 0; i < values.length; i++) {
-            if (values.hasOwnProperty(prop)) {
-              if (validator.isEmpty(values[i][`${prop}`])) {
-                notValid = true;
-                break;
-              }
-            } else {
-              notValid = true;
-              break;
-            }
-          }
-          return notValid;
-        }
-      }
-    })
-  );
 };
