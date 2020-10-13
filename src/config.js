f28ad8bd7a708ebd2e03fb1547ab2172c8bb25a2
pezhmanparsaee <p.parsaee@gmail.com>
2020-10-13 17:58:41 +0330

    change database url based on NODE_ENV var

--- src/config.js
+++ src/config.js
@@ -7,9 +7,15 @@ if (result.error) {
   throw result.error;
 }
 
-const parsedUrl = url.parse(
-  process.env.CONNECTION_STRING || 'mongodb://localhost:27017/reactor'
-);
+let dbUrl;
+
+if (process.env.NODE_ENV === 'test') {
+  dbUrl = 'mongodb://localhost:27017/reactor_test';
+} else {
+  dbUrl = process.env.CONNECTION_STRING || 'mongodb://localhost:27017/reactor';
+}
+
+const parsedUrl = url.parse(dbUrl);
 
 module.exports = {
   app: {
@@ -22,6 +28,6 @@ module.exports = {
     name: parsedUrl.pathname.substr(1),
     user: parsedUrl.auth ? parsedUrl.auth.split(':')[0] : null,
     password: parsedUrl.auth ? parsedUrl.auth.split(':')[1] : null,
-    url: process.env.CONNECTION_STRING
+    url: dbUrl
   }
 };
