729138bf104d96c5c9dd729e9215a679386ff18f
pezhmanparsaee <p.parsaee@gmail.com>
2020-10-13 17:59:49 +0330

    add an integration test for GET /api/v1/customer

---
+++ tests/integration/customer.test.js
@@ -0,0 +1,49 @@
+const request = require('supertest');
+const expect = require('chai').expect;
+const server = require('../../src/app');
+const dbConnection = require('../../src/db/connection');
+
+describe('/api/v1/customer', () => {
+  before(function(done) {
+    dbConnection.getInstance().then(() => {
+      done();
+    });
+  });
+
+  after(function(done) {
+    dbConnection.getInstance().then(dbInstance => {
+      dbInstance.close(() => {
+        done();
+      });
+    });
+  });
+
+  afterEach(function(done) {
+    dbConnection
+      .getDb()
+      .collection('customers')
+      .deleteMany({})
+      .then(() => {
+        done();
+      });
+  });
+
+  describe('GET /', () => {
+    it('should return all customers', async () => {
+      await dbConnection
+        .getDb()
+        .collection('customers')
+        .insertMany([
+          { fullName: 'Pezhman Parsaee' },
+          { fullName: 'john Doe' }
+        ]);
+
+      const res = await request(server).get('/api/v1/customer');
+
+      expect(res.status).to.equal(200);
+      expect(res.body.status).to.equal(true);
+      expect(Array.isArray(res.body.payload)).to.equal(true);
+      expect(res.body.payload.length).to.equal(2);
+    });
+  });
+});
