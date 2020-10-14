ae5c4ea6dc167f8386dc9cda770da33b11efa09f
pezhmanparsaee <p.parsaee@gmail.com>
2020-10-14 13:00:09 +0330

    refactor product repository

--- src/area/product/repository.js
+++ src/area/product/repository.js
@@ -1,40 +1,37 @@
 const dbConnection = require('../../db/connection');
-const opStatusGenerator = require('../../common/helpers/op-status-generator');
 const { COLLECTIONS } = require('../../common/enums');
 
-const makeProductRepository = function(dependencies) {
-  async function getAll() {
-    const db = dependencies.dbConnection.getDb();
+function makeGetAll({ dbConnection }) {
+  return async function getAll() {
+    const db = dbConnection.getDb();
 
-    const res = await db
+    const products = await db
       .collection(COLLECTIONS.PRODUCTS)
       .find()
       .toArray();
 
-    return opStatusGenerator({
-      status: true,
-      payload: res
-    });
-  }
+    return products;
+  };
+}
 
-  async function add(product) {
-    const db = dependencies.dbConnection.getDb();
+function makeAdd({ dbConnection }) {
+  return async function add(product) {
+    const db = dbConnection.getDb();
 
     const res = await db.collection(COLLECTIONS.PRODUCTS).insertOne(product);
 
-    return opStatusGenerator({
-      status: res.result.ok === 1,
-      payload: res.result.ok === 1 ? res.ops[0] : null
-    });
-  }
+    return res.result.ok === 1;
 
-  return {
-    getAll,
-    add
+    // return opStatusGenerator({
+    //   status: res.result.ok === 1,
+    //   payload: res.result.ok === 1 ? res.ops[0] : null
+    // });
   };
+}
+
+module.exports = {
+  getAll: makeGetAll({ dbConnection }),
+  add: makeAdd({ dbConnection }),
+  makeGetAll,
+  makeAdd
 };
-
-const repository = makeProductRepository({ dbConnection });
-
-module.exports = repository;
-exports.makeProductRepository = makeProductRepository;
